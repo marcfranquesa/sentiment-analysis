fc3db3419a3c41aa6b50a18e873973fb835028bf
Marc Franquesa <110655741+marcfranquesa@users.noreply.github.com>
2024-06-21 17:34:26 +0200

    Add post endpoint

--- server/main.py
+++ server/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import pipeline
+from typing import Dict
 
+sentiment_pipeline = pipeline("sentiment-analysis")
 app = FastAPI()
 
 
+class Text(BaseModel):
+    text: str
+
+@app.post("/")
+async def inference(text: Text) -> Dict[str, str]:
+    out = sentiment_pipeline(text.text)
+    return {"LABEL": out[0]["label"]}
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def root() -> Dict[str, str]:
+    return "hi"
