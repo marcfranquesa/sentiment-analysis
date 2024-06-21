c9b8c84bd77a277b8c1c9b1c6c49aa3023f248ec
Marc Franquesa <110655741+marcfranquesa@users.noreply.github.com>
2024-06-21 18:57:31 +0200

    Specify api origins

--- server/main.py
+++ server/main.py
@@ -2,9 +2,23 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import pipeline
 from typing import Dict
+from fastapi.middleware.cors import CORSMiddleware
 
-sentiment_pipeline = pipeline("sentiment-analysis", model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
+
+sentiment_pipeline = pipeline(
+    "sentiment-analysis",
+    model="distilbert/distilbert-base-uncased-finetuned-sst-2-english"
+)
 app = FastAPI()
+
+origins = ["http://localhost:8080", "null"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
 
 
 class Text(BaseModel):
@@ -12,9 +26,20 @@ class Text(BaseModel):
 
 @app.post("/")
 async def inference(text: Text) -> Dict[str, str]:
-    out = sentiment_pipeline(text.text)
-    return {"LABEL": out[0]["label"]}
+    try:
+        out = sentiment_pipeline(text.text)
+        label = out[0]["label"]
+        
+        returnLabel = "Negative 😔"
+        if label == "POSITIVE":
+            returnLabel = "Positive 😊"
+
+        return {"LABEL": returnLabel}
+    
+
+    except Exception as e:
+        return {"error": str(e)}
 
 @app.get("/")
 async def root() -> Dict[str, str]:
-    return "hi"
+    return {"message": "hi"}
